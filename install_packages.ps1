a7fc2cc70a9902754e682a3e3264a7303003adcf
CommanderKeynes <46945903+CommanderKeynes@users.noreply.github.com>
2023-07-12 13:39:25 -0500

    Created package install script.

---
+++ install_packages.ps1
@@ -0,0 +1,29 @@
+
+#Requires -RunAsAdministrator
+
+$packages = @(
+    "Git.Git",
+    "GitHub.GitHubDesktop",
+    "Microsoft.VisualStudioCode",
+    "Bitwarden.Bitwarden",
+    "Mozilla.Firefox",
+    "Valve.Steam",
+    "Adobe.Acrobat.Reader.64-bit",
+    "Python.Python.3.12",
+    "RProject.R",
+    "Hashicorp.Terraform",
+    "TheDocumentFoundation.LibreOffice",
+    "MiKTeX.MiKTeX",
+    "CCL.NetLogo",
+    "vim.vim",
+    "GNU.Emacs",
+    "Docker.DockerDesktop",
+    "GoLang.Go.1.18",
+    "Rustlang.Rustup",
+    "GIMP.GIMP",
+    "OpenJS.NodeJS",
+    "Discord.Discord")
+
+foreach ($package in $packages){
+    winget install -e --id $package 
+}
