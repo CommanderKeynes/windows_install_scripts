dc52a13dc9ad6437817b53ede777939ff0385f21
CommanderKeynes <46945903+CommanderKeynes@users.noreply.github.com>
2024-05-05 22:17:18 -0500

    DRY

--- chocolatey.rb
+++ chocolatey.rb
@@ -1,80 +1,9 @@
-
-chocolatey_package 'git' do
-    action :install
-end
-
-chocolatey_package "bitwarden" do
-	action :install
-end
-
-chocolatey_package "discord" do
-	action :install
-end
-
-chocolatey_package "docker-desktop" do
-	action :install
-end
-
-chocolatey_package "gimp" do
-	action :install
-end
-
-chocolatey_package "github-desktop" do
-	action :install
-end
-
-chocolatey_package "git" do
-	action :install
-end
-
-chocolatey_package "gcloudsdk" do
-	action :install
-end
-
-chocolatey_package "terraform" do
-	action :install
-end
-
-chocolatey_package "miktex" do
-	action :install
-end
-
-chocolatey_package "firefox" do
-	action :install
-end
-
-chocolatey_package "rust" do
-	action :install
-end
-
-chocolatey_package "r" do
-	action :install
-end
-
-chocolatey_package "steam" do
-	action :install
-end
-
-chocolatey_package "nodejs" do
-	action :install
-end
-
-chocolatey_package "netlogo" do
-	action :install
-end
-
-chocolatey_package "vscode" do
-	action :install
-end
-
-chocolatey_package "adobereader" do
-	action :install
-end
-
-chocolatey_package "python" do
-	action :install
-end
-
-chocolatey_package "strawberryperl" do
-    action :install
+packages_to_install = [
+	"bitwarden", "discord", "docker-desktop", "gimp", "github-desktop", "git", 
+	"gcloudsdk", "terraform", "miktex", "firefox", "rust", "r", "steam", 
+	"nodejs", "netlogo", "vscode", "adobereader", "python", "strawberryperl"]
+packages_to_install.each do |package_to_install|
+	chocolatey_package "#{package_to_install}" do
+		action :install
+	end
 end
