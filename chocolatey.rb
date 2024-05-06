fdcb15020662a98fafc1dcd473e3dd6cb73863d4
CommanderKeynes <46945903+CommanderKeynes@users.noreply.github.com>
2024-05-05 21:31:51 -0500

    Move installs to puppet

---
+++ chocolatey.rb
@@ -0,0 +1,80 @@
+
+chocolatey_package 'git' do
+    action :install
+end
+
+chocolatey_package "bitwarden" do
+	action :install
+end
+
+chocolatey_package "discord" do
+	action :install
+end
+
+chocolatey_package "docker-desktop" do
+	action :install
+end
+
+chocolatey_package "gimp" do
+	action :install
+end
+
+chocolatey_package "github-desktop" do
+	action :install
+end
+
+chocolatey_package "git" do
+	action :install
+end
+
+chocolatey_package "gcloudsdk" do
+	action :install
+end
+
+chocolatey_package "terraform" do
+	action :install
+end
+
+chocolatey_package "miktex" do
+	action :install
+end
+
+chocolatey_package "firefox" do
+	action :install
+end
+
+chocolatey_package "rust" do
+	action :install
+end
+
+chocolatey_package "r" do
+	action :install
+end
+
+chocolatey_package "steam" do
+	action :install
+end
+
+chocolatey_package "nodejs" do
+	action :install
+end
+
+chocolatey_package "netlogo" do
+	action :install
+end
+
+chocolatey_package "vscode" do
+	action :install
+end
+
+chocolatey_package "adobereader" do
+	action :install
+end
+
+chocolatey_package "python" do
+	action :install
+end
+
+chocolatey_package "strawberryperl" do
+    action :install
+end
