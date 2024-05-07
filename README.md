9ae6f5a118fca0c9ee3c4e68d5b3eb56ced8df20
CommanderKeynes <46945903+CommanderKeynes@users.noreply.github.com>
2024-05-06 22:14:28 -0500

    Add hyper v config

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 Desktop
 Start-DscConfiguration -Path .\Desktop -Verbose -Wait
 ```
+
+```powershell
+Remove-DscConfigurationDocument -Stage Pending
+```
--- choco.ps1
+++ choco.ps1
@@ -3,12 +3,55 @@ Configuration Desktop {
     # Import the module that contains the File resource.
     Import-DscResource -ModuleName PsDesiredStateConfiguration
     Import-DscResource -ModuleName cChoco
+    Import-DscResource -ModuleName 'xHyper-V'
 
     # The Node statement specifies which targets to compile MOF files for, when
     # this configuration is executed.
     Node 'localhost' {
+        $VhdPath = "C://"
+        $VMName = "nix_os_vm"
+        $diskNameOS = "$VMName-DiskOS.vhdx"
+        $diskNameExtra1 = "$VMName-Disk1.vhdx"
 
-        cChocoPackageInstallerSet installGit
+        WindowsFeature Hyper-V {
+            Ensure = 'Present'
+            Name = "Hyper-V"
+        }
+
+        WindowsFeature HyperVPowerShell
+        {
+            Ensure = 'Present'
+            Name   = 'Hyper-V-PowerShell'
+        }
+
+        xVHD DiskOS
+        {
+            Name             = $diskNameOS
+            Path             = $VhdPath
+            Generation       = 'vhdx'
+            MaximumSizeBytes = 20GB
+            Ensure           = 'Present'
+        }
+
+        xVHD Disk1
+        {
+            Name             = $diskNameExtra1
+            Path             = $VhdPath
+            Generation       = 'vhdx'
+            MaximumSizeBytes = 20GB
+            Ensure           = 'Present'
+        }
+
+        xVMHyperV NewVM
+        {
+            Ensure     = 'Present'
+            Name       = "nix_os_vm"
+            VhdPath    = Join-Path $VhdPath -ChildPath $diskNameOS
+            Generation = 1
+            DependsOn  = '[xVHD]DiskOS'
+        }
+
+        cChocoPackageInstallerSet installChocoPackages
         {
                 Ensure = 'present'
                 Name = @(
